81fae2aa9821721265adf56f1fc83c6aa2be0f45
jamesb <experimentallearning0@gmail.com>
2021-08-18 21:07:07 +0100

    - Added: Added some random testing data.

---
+++ src/TestData.tsx
@@ -0,0 +1,30 @@
+import DisplayAt from "./Components/Base/DisplayAt";
+
+interface IComponentData {
+  displayAt: DisplayAt;
+}
+
+  interface IHtmlComponentData extends IComponentData {
+    text: string;
+  }
+
+type ComponentData = IHtmlComponentData;
+
+  export interface ElementData {
+    components: ComponentData[];
+  }
+
+export const data: ElementData[] = [
+{
+components: [
+            {
+displayAt: DisplayAt.Question,
+             text: "What is the capital of England?",
+            },
+            {
+displayAt: DisplayAt.Answer,
+           text: "London",
+            },
+],
+},
+];
